0f7e64aa5e14442b90d937ce0c32145f5f625d2e
chengxiang <1923136366@qq.com>
2022-08-14 15:08:18 +0800

    create project

---
+++ src/test/java/com/yamy/shop/YamyShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yamy.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YamyShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
